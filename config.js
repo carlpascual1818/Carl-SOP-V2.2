074e8f6f5bb9558423dd75b5345474dcf83ba2ba
carlpascual1818 <carlpascual1818@gmail.com>
2026-02-18 05:28:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // This file is safe to be public because it uses the anon key.
 // Do NOT put the service role key here.
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://wkwgyoowhfhkyqwlufup.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indrd2d5b293aGZoa3lxd2x1ZnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNDQwMTIsImV4cCI6MjA4NjkyMDAxMn0.ArSqtBC9K_268UnAfwTiqgYoJXGoXDmp1n9J2ISzgLE"
 };
